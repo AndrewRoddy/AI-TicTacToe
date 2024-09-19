94d8b5d2afe2e36b21cbdb24fc147c078d6739d7
Andrew Roddy <andrewroddy7@gmail.com>
2024-09-19 08:24:26 -0400

    v0.00.02 Made drawBoard function

--- main.cpp
+++ main.cpp
@@ -2,7 +2,31 @@
 #include <vector>
 
 using std::cout; using std::cin; using std::endl;
+using std::vector;
+
+vector <vector <char> > board = {{'_','_','_'},{'_','_','_'},{'_','_','_'}};
+
+void drawBoard();
 
 int main() {
-    cout << "HI" << endl;
+    drawBoard();
 }
+
+void drawBoard() {
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            cout << ' ' << board[i][j]; // Draws X or O or _
+            if (j < 2) { // Checks to draw line between icons and not after
+                cout << " |";
+            }
+        }
+        cout << endl;
+    }
+}
+
+// Personal Notes
+/*
+ - Make sure to include <vector> and std::vector or using std::vector.
+ - 2D Vectors are generated using vector<vector<type>> var = {{},{},{}};
+*/
+
