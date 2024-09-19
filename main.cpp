72eb44afc98f96711481924f5f586b44599b67eb
Andrew Roddy <andrewroddy7@gmail.com>
2024-09-19 09:53:50 -0400

    v0.00.03 Creates function to make moves

--- main.cpp
+++ main.cpp
@@ -4,24 +4,53 @@
 using std::cout; using std::cin; using std::endl;
 using std::vector;
 
-vector <vector <char> > board = {{'_','_','_'},{'_','_','_'},{'_','_','_'}};
+vector <vector <char> > board = {{'1','2','3'},{'4','5','6'},{'7','8','9'}};
 
 void drawBoard();
+void makeMove(int move, char player);
+void printCoords(int move);
 
 int main() {
+    char player = 'X';
+    int move = -1;
+    drawBoard();
+    cout << endl << "Player " << player << " turn: " << endl;
+    cin >> move;
+    makeMove(move, player);
     drawBoard();
 }
 
+// Draws a TicTacToe board
 void drawBoard() {
     for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 3; j++) {
             cout << ' ' << board[i][j]; // Draws X or O or _
-            if (j < 2) { // Checks to draw line between icons and not after
-                cout << " |";
-            }
+            if (j < 2) {cout << " |";} // Draws vertical lines
         }
-        cout << endl;
+        if (i < 2) { // Draws Horizontal lines
+            cout << endl << "-- - - - --" << endl;
+        }
     }
+}
+
+void makeMove(int move, char player){
+    int col = (int) ((move-1)/3);
+    int row = (move-1) % 3;
+    if (move >= 1 && move <= 9){
+        if(board[col][row] == 'X' || board[col][row] == 'O'){
+            cout << "You can't move there!" << endl;
+        } else {
+            board[col][row] = player;
+        }
+    } else {
+        cout << "You can't move there!" << endl;
+    }
+}
+
+void printCoords(int move){
+    cout << '(' << (int) ((move-1)/3); // X Coordinate
+    cout << ',';
+    cout << (move-1) % 3 << ')'; // Y Coordinate
 }
 
 // Personal Notes
